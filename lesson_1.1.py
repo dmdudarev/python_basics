2a4035b67b293e6169c1a6ed8599036216ce1272
dmitrydudarev <musay@yandex.ru>
2021-08-17 23:25:12 +0400

    appdate lesson_1.1.py

---
+++ lesson_1.1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+sname = input("Enter your surname: ")
+print (f"Hello, {name} {sname}!")
+age = int(input("Enter your age: "))
+age20 = age + 20
+print(f"Через 20 лет Вам будет уже {age20}!")
