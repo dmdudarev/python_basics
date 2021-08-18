14226ce76e60ad3041fa1ad375c8bafd5fc1d080
dmitrydudarev <musay@yandex.ru>
2021-08-18 09:28:24 +0400

    update all

--- lesson_1.2.py
+++ lesson_1.2.py
@@ -2,4 +2,4 @@ tm_sec = int(input("Введите время в секундах: "))
 hour = tm_sec // 3600
 minut = (tm_sec % 3600) // 60
 sec = (tm_sec % 3600) % 60
-print (f"{hour:02}:{minut:02}:{sec:02}")
+print(f"{hour:02}:{minut:02}:{sec:02}")
